5a492b1cdada568004d79513e47aa01445b24043
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-25 22:32:45 +0200

    add download information

---
+++ IAnalogy/DownloadInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using Analogy.LogViewer.RegexParser.IAnalogy;
+
+namespace Analogy.LogViewer.KamaResearch.IAnalogy
+{
+    public class DownloadInformation : Analogy.LogViewer.Template.AnalogyDownloadInformation
+    {
+        protected override string RepositoryURL { get; set; } = "https://api.github.com/repos/Analogy-LogViewer/Analogy.LogViewer.KamaResearch";
+        public override TargetFrameworkAttribute CurrentFrameworkAttribute { get; set; } = (TargetFrameworkAttribute)Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(TargetFrameworkAttribute));
+
+        public override Guid FactoryId { get; set; } = KamaFactories.Id;
+        public override string Name { get; set; } = "Kama Research And Development";
+
+        private string? _installedVersionNumber;
+        public override string InstalledVersionNumber
+        {
+            get
+            {
+                if (_installedVersionNumber != null)
+                {
+                    return _installedVersionNumber;
+                }
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+                _installedVersionNumber = fvi.FileVersion;
+                return _installedVersionNumber;
+            }
+        }
+    }
+}
